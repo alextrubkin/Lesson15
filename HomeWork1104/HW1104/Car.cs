03bf5b91f0e7738178973fd4e69b8e9fb9635b69
e.repin <mr.evgeniy.repin@gmail.com>
2020-04-13 18:33:04 +0300

    comment

---
+++ HomeWork1104/HW1104/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW1104
+{
+    public class Car
+    {
+        public Car(string mark, string model, double engineVolume, string fuelType)
+        {
+            Mark = mark;
+            Model = model;
+            EngineVolume = engineVolume;
+            FuelType = fuelType;
+        }
+        public string Mark { get; set; }
+        public string Model { get; set; }
+        public double EngineVolume { get; set; }
+        public string FuelType { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Mark}, {Model}, {EngineVolume}, {FuelType}";
+        }
+    }
+}
---
+++ HomeWork1104/HW1104/Cars.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW1104
+{
+    public class Cars<T> : IEnumerable
+    {
+        public Car[] cars = new Car[3];
+
+        public Car this[int index]
+        {
+            get { return cars[index]; }
+            set { cars[index] = value; }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var car in cars)
+            {
+                yield return car;
+            }
+        }
+    }
+}
