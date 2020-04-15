00ade027eccd6c6b4739c4b8206a6e013ca50423
Oleksii <alexmkh89@gmail.com>
2020-04-15 22:52:51 +0300

    CreditCards

---
+++ Homework15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_16
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditCard<string> creditCard1 = new CreditCard<string>("string id",200);
+            CreditCard<int> creditCard2 = new CreditCard<int>(456123, 500);
+
+            ArrayList cards = new ArrayList() {creditCard1, creditCard2 };
+
+            foreach (var item in cards)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
